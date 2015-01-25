3c8bb6173c11c96e2a750fc86f9c4ce544bc8ef3
sydehlinger <sydney.ehlinger@gmail.com>
2015-01-24 23:18:41 -0600

    Added users

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var nicknames = [];
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -12,16 +13,33 @@ http.listen(2000, function(){
 
 io.on('connection', function(socket){
     console.log('a user connected');
+
+    socket.on('new user', function(data, callback){
+      if (nicknames.indexOf(data) != -1){
+        callback(false);
+      } else{
+        callback(true);
+        socket.nickname = data;
+        nicknames.push(socket.nickname);
+        updateNicknames();
+      }
+    });
+
+    function updateNicknames(){
+      io.emit('usernames', nicknames);
+    }
+
+    socket.on('chat message', function(msg){
+      var time = Date();
+      console.log('User: ' + msg);
+      io.emit('chat message', socket.nickname + " : " + msg);
+    });
+
     socket.on('disconnect', function(){
+      if(!socket.nickname) return;
+      nicknames.splice(nicknames.indexOf(socket.nickname), 1);
+      updateNicknames();
       console.log('user disconnected')
-  });
-  socket.on('chat message', function(msg){
-    var time = Date();
-    console.log('User: ' + msg);
-    io.emit('chat message', "User " + "[" + time + "]" + " : " + msg);
-  });
-  socket.on('username', function(msg){
-    console.log('Username changed to ' + msg);
-    io.emit('username changed', + msg);
-  });
+    });
+
 });
